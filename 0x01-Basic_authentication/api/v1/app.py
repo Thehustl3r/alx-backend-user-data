18454df01249298694b84959ea3f77767ae88014
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-15 04:34:45 +0200

    addition of auth in the main aoo

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,7 +21,7 @@ if os.getenv('AUTH_TYPE'):
 
 @app.before_request
 def before_request():
-    """fdsfsfdfsfdfsfdf"""
+    """the function that is processed before we process the request"""
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/',
