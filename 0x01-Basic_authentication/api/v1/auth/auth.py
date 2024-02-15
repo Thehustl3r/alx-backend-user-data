6739e22717154b529b8d94ef5f44691482196d87
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-15 02:23:57 +0200

    create auth folder

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env/ python3
+"""The module for auth"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """The class to hold all the authentication"""
+
+    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
+        """The function that will check if the auth is required"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """the function"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Function the user """
+        return None
