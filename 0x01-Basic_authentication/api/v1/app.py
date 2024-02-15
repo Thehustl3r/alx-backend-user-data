f786e063818beccac65257e2563f4162923c8dea
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-15 15:38:32 +0200

    Creation of Basic authentication class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -16,7 +17,10 @@ app.register_blueprint(app_views)
 
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 if os.getenv('AUTH_TYPE'):
-    auth = Auth()
+    if os.getenv('AUTH_TYPE') == 'basic_auth':
+        auth = BasicAuth()
+    else:
+        auth = Auth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""basic_auth.py"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """A basic clasll of the authentication"""
+    pass
