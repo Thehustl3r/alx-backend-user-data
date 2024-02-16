ba1134c39b103013c609c25eea768287df86c54a
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-16 03:10:51 +0200

    update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,14 +37,13 @@ def before_request():
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.session_cookie(request) is None \
+            and auth.authorization_header(request) is None:
         abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.session_cookie(request) is None:
-        abort(401)
+
     request.current_user = auth.current_user(request)
 
 
@@ -65,7 +64,7 @@ def unauthorized(error) -> str:
 
 
 @app.errorhandler(403)
-def unauthorized(error) -> str:
+def forbidden(error) -> str:
     """ Forbidden handler
     """
     response = jsonify({"error": "Forbidden"})
